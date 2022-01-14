0f172deca97af76b7592511cb1363bd0d90bbd3d
Muriel Damazio <murieldps@gmail.com>
2022-01-14 08:49:34 -0300

    AuthContext done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { AuthProvider } from './src/contexts/AuthContext'
+import { AuthProvider } from '../src/contexts/AuthContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
