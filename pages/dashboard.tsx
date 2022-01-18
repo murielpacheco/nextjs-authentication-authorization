71bcf586ae3b062e63c161646d3a9fe9a5b7aea5
Muriel Damazio <murieldps@gmail.com>
2022-01-18 09:29:09 -0300

    Feat: Permission hook useCan

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../src/contexts/AuthContext"
+import { useCan } from "../src/hooks/useCan"
 import { setupAPIClient } from "../src/services/api"
 import { api } from "../src/services/apiClient"
 import { withSSRAuth } from "../src/utils/withSSRAuth"
@@ -7,13 +8,21 @@ import { withSSRAuth } from "../src/utils/withSSRAuth"
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
 
+  const userCanSeeMetrics = useCan({
+    roles: ['administrator', 'editor']
+  })
+
   useEffect(() => {
     api.get('/me')
       .then(response => console.log(response))
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
   )
 }
 
@@ -21,7 +30,7 @@ export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx)
   const response = await apiClient.get('/me')
 
-  
+
   return {
     props: {}
   }
